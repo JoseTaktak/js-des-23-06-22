0ae2c0c1a37aa26e640523802cc36bea014a7625
Jose Taktak <jtaktak@outlook.com>
2022-06-25 19:05:20 -0300

    cargo mod app

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Constructor de productos con el método de Sumar el IVA
 class Productos{
-    constructor(Cod, title, precio, src) {
-        this.Cod = id;
+    constructor(id, title, precio, img) {
+        this.id = id;
         this.title = title;
         this.precio = precio;
-        this.src = src;
+        this.img = img;
     }
     sumarIva *=1.21
 }
@@ -12,15 +12,15 @@ class Productos{
 // Array de productos para hacer el push de los productos
 const productos = [];
 
-productos.push(new Productos(1, "Azuquitas", 100, src="image/productos/azuqitas color.jpg"));
-productos.push(new Productos(2, "Baston Acqua", 60, src="image/Productos/Baston-acqua.jpg" alt="Baston-acqua"));
-productos.push(new Productos(3, "Baston Multi", 65, src="image/Productos/Baston-multi.jpg" alt="Baston-multi"));
-productos.push(new Productos(4, "Baston Rosa", 60, src="image/Productos/Baston-rosa.jpg" alt="Baston-rosa"));
-productos.push(new Productos(5, "Meloncito Amarillo", 50, src="image/Productos/Meloncito-amarillo.jpg" alt="Meloncito-Amarillo"));
-productos.push(new Productos(6, "Meloncito Azul", 50, src="image/Productos/Meloncito-Azul.jpg" alt="Meloncito-Azul"));
-productos.push(new Productos(7, "Meloncito Rosa", 50, src="image/Productos/Meloncito-Rosa.jpg" alt="Meloncito-Rosa"));
-productos.push(new Productos(8, "Pirulin Amarillo", 80, src="image/Productos/Pirulin-amarillo.jpg" alt="Pirulin-amarillo"));
-productos.push(new Productos(1, "Pirulin Rosa", 80, src="image/Productos/Pirulin-rosa.jpg" alt="Pirulin-rosa"));
+productos.push(new Productos(1, "Azuquitas", 100, <img src="image/productos/azuqitas color.jpg"></img>));
+productos.push(new Productos(2, "Baston Acqua", 60, <img src="image/Productos/Baston-acqua.jpg" alt="Baston-acqua"></img>));
+productos.push(new Productos(3, "Baston Multi", 65, <img src="image/Productos/Baston-multi.jpg" alt="Baston-multi"></img>));
+productos.push(new Productos(4, "Baston Rosa", 60, <img src="image/Productos/Baston-rosa.jpg" alt="Baston-rosa"></img>));
+productos.push(new Productos(5, "Meloncito Amarillo", 50, <img src="image/Productos/Meloncito-amarillo.jpg" alt="Meloncito-Amarillo"></img>));
+productos.push(new Productos(6, "Meloncito Azul", 50, <img src="image/Productos/Meloncito-Azul.jpg" alt="Meloncito-Azul"></img>));
+productos.push(new Productos(7, "Meloncito Rosa", 50, <img src="image/Productos/Meloncito-Rosa.jpg" alt="Meloncito-Rosa"></img>));
+productos.push(new Productos(8, "Pirulin Amarillo", 80, <img src="image/Productos/Pirulin-amarillo.jpg" alt="Pirulin-amarillo"></img>));
+productos.push(new Productos(1, "Pirulin Rosa", 80, <img src="image/Productos/Pirulin-rosa.jpg" alt="Pirulin-rosa"></img>));
 
 let container = document.getElementById("container");
 
@@ -31,7 +31,7 @@ for (const producto of productos) {
     let div = document.createElement("div");
     div.className = "card"
     div.innerHTML = ` 
-        <img class="img-card" src="${producto.src}" alt="">
+        <img class="img-card" img="${producto.img}" alt="">
     <p class="id-card">Cod: ${producto.Cod}</p>
     <p class="title-card">${producto.title}</p>
     <p class="precio-card">$${producto.precio}</p>
